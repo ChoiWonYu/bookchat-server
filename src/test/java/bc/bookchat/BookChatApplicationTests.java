280e3f22e0c9850a09d3b798089053f13b8f0b4d
ChoiWonYu <phillip07290@naver.com>
2023-07-17 22:27:13 +0900

    setting:  init spring project

---
+++ src/test/java/bc/bookchat/BookChatApplicationTests.java
@@ -0,0 +1,13 @@
+package bc.bookchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
