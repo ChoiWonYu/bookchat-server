5e7f133326a206da0658315673a043ce14e02a54
ChoiWonYu <phillip07290@naver.com>
2023-08-13 10:01:02 +0900

    feat: member entity

---
+++ src/main/java/bc/bookchat/member/entity/Member.java
@@ -0,0 +1,36 @@
+package bc.bookchat.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_id")
+  private Long id;
+
+  @Column(unique = true)
+  private String email;
+
+  private String password;
+
+  private String userName;
+
+  @Builder
+  public Member(String email, String password, String userName) {
+    this.email=email;
+    this.password=password;
+    this.userName=userName;
+  }
+}
