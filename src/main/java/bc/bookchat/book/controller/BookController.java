db8e963ba75e393c046ba3013b673b1571ce1ec2
ChoiWonYu <phillip07290@naver.com>
2023-08-15 16:45:44 +0900

    feat: get books api

--- src/main/java/bc/bookchat/book/controller/BookController.java
+++ src/main/java/bc/bookchat/book/controller/BookController.java
@@ -1,8 +1,31 @@
 package bc.bookchat.book.controller;
 
+import bc.bookchat.book.controller.dto.BookInfo;
+import bc.bookchat.book.controller.dto.GetBookQuery;
+import bc.bookchat.book.service.BookService;
+import bc.bookchat.common.response.PageResponse;
+import bc.bookchat.common.response.ResponseHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
+
+  private final BookService bookService;
+
+  @GetMapping()
+  public ResponseEntity<Object> getBooks(GetBookQuery query){
+
+    PageResponse<BookInfo> response=bookService.getBooksPageResponse(query);
+    return ResponseHandler.generateResponseWithoutMsg(HttpStatus.OK,response);
+  }
+
 
 }
--- src/main/java/bc/bookchat/book/service/BookService.java
+++ src/main/java/bc/bookchat/book/service/BookService.java
@@ -1,8 +1,63 @@
 package bc.bookchat.book.service;
 
+import bc.bookchat.book.controller.dto.BookInfo;
+import bc.bookchat.book.controller.dto.GetBookQuery;
+import bc.bookchat.book.controller.dto.KakaoResponse;
+import bc.bookchat.common.response.CommonPageInfo;
+import bc.bookchat.common.response.PageResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class BookService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${kakao.baseurl}")
+    private String baseUrl;
+
+    @Value("${kakao.secret}")
+    private String kakaoSecretKey;
+
+    public PageResponse<BookInfo> getBooksPageResponse(GetBookQuery query) {
+        KakaoResponse response= requestToApi(query);
+        return convertToCommonPageResponse(query,response);
+    }
+
+    private KakaoResponse requestToApi(GetBookQuery query) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "KakaoAK " + kakaoSecretKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity request = new HttpEntity(headers);
+
+        String url = query.toUrl(baseUrl);
+
+
+        return restTemplate.exchange(url, HttpMethod.GET, request, KakaoResponse.class).getBody();
+    }
+
+    private PageResponse<BookInfo> convertToCommonPageResponse(GetBookQuery query,KakaoResponse kakaoResponse){
+
+        List<BookInfo> bookInfoList= Arrays.stream(kakaoResponse.getDocuments()).map(BookInfo::toBookInfo).toList();
+        CommonPageInfo pageInfo=kakaoResponse.getMeta()
+                .toCommonPageInfo(query.getPage(),query.getSize());
+
+        return new PageResponse(bookInfoList,pageInfo);
+
+    }
+
 
 }
