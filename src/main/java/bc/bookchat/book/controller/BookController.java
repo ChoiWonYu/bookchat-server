ae21da6d26cf28ad90f1d066ed76bf302706b033
ChoiWonYu <phillip07290@naver.com>
2023-08-15 23:02:12 +0900

    feat: validate request query params

--- src/main/java/bc/bookchat/book/controller/BookController.java
+++ src/main/java/bc/bookchat/book/controller/BookController.java
@@ -5,8 +5,8 @@ import bc.bookchat.book.controller.dto.GetBookQuery;
 import bc.bookchat.book.service.BookService;
 import bc.bookchat.common.response.PageResponse;
 import bc.bookchat.common.response.ResponseHandler;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +21,10 @@ public class BookController {
   private final BookService bookService;
 
   @GetMapping()
-  public ResponseEntity<Object> getBooks(GetBookQuery query){
+  public ResponseEntity<Object> getBooks(@Valid GetBookQuery query){
 
     PageResponse<BookInfo> response=bookService.getBooksPageResponse(query);
     return ResponseHandler.generateResponseWithoutMsg(HttpStatus.OK,response);
   }
-
 
 }
