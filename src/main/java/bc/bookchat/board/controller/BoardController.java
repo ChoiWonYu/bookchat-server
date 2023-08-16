dd540da874512e5ab2b14491ded20470685e067d
ChoiWonYu <phillip07290@naver.com>
2023-08-16 16:14:35 +0900

    feat: get book boards api

--- src/main/java/bc/bookchat/board/controller/BoardController.java
+++ src/main/java/bc/bookchat/board/controller/BoardController.java
@@ -1,10 +1,25 @@
 package bc.bookchat.board.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import bc.bookchat.board.controller.dto.BoardPaginationQuery;
+import bc.bookchat.board.controller.dto.CommonBoardResponse;
+import bc.bookchat.board.service.BoardService;
+import bc.bookchat.common.response.PageResponse;
+import bc.bookchat.common.response.ResponseHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/boards")
+@RequiredArgsConstructor
 public class BoardController {
 
+    private final BoardService boardService;
+
+    @GetMapping("/{isbn}")
+    public ResponseEntity<Object> getBookBoards(@PathVariable Long isbn, BoardPaginationQuery query){
+        PageResponse<CommonBoardResponse>response=boardService.getBookBoards(isbn,query);
+        return ResponseHandler.generateResponseWithoutMsg( HttpStatus.OK,response);
+    }
 }
--- src/main/java/bc/bookchat/board/service/BoardService.java
+++ src/main/java/bc/bookchat/board/service/BoardService.java
@@ -1,7 +1,51 @@
 package bc.bookchat.board.service;
 
+import bc.bookchat.board.controller.dto.BoardPaginationQuery;
+import bc.bookchat.board.controller.dto.CommonBoardResponse;
+import bc.bookchat.board.entity.Board;
+import bc.bookchat.board.repository.BoardRepository;
+import bc.bookchat.book.repository.BookRepository;
+import bc.bookchat.book.service.BookService;
+import bc.bookchat.common.response.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BookRepository bookRepository;
+    private final BookService bookService;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public PageResponse<CommonBoardResponse> getBookBoards(Long isbn, BoardPaginationQuery query) {
+        if (bookRepository.findById(isbn).isEmpty()) {
+            bookService.createBook(isbn);
+        }
+
+        Page<Board> boardsPage = getPaginationBoards(isbn, query);
+        List<CommonBoardResponse> results = boardsPage.stream().map(Board::toDto).toList();
+        return new PageResponse<>(results, query.toPageInfo(boardsPage));
+    }
+
+    private Page<Board> getPaginationBoards(Long isbn, BoardPaginationQuery query) {
+        PageRequest pageRequest = makePageRequest(query);
+
+        if (query.hasKeyword()) {
+            return boardRepository.findByIsbnAndTitleContaining(isbn,
+                    query.getKeyword(), pageRequest);
+        }
+        return this.boardRepository.findByIsbn(isbn, pageRequest);
+    }
+
+    private PageRequest makePageRequest(BoardPaginationQuery boardPaginationDto) {
+        return PageRequest.of(boardPaginationDto.getPage() - 1,
+                boardPaginationDto.getSize());
+    }
 }
