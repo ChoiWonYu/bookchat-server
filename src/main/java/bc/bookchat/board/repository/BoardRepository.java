847e6e5c73af8279a4f2f4c348558d3efe44e567
ChoiWonYu <phillip07290@naver.com>
2023-08-16 16:12:53 +0900

    feat: find by isbn method, and title containing method

--- src/main/java/bc/bookchat/board/repository/BoardRepository.java
+++ src/main/java/bc/bookchat/board/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package bc.bookchat.board.repository;
 
 import bc.bookchat.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface BoardRepository extends JpaRepository<Board, UUID> {
+    Page<Board> findByIsbnAndTitleContaining(Long isbn,String keyword, Pageable pageRequest);
+    Page<Board> findByIsbn(Long isbn,Pageable pageRequest);
 }
