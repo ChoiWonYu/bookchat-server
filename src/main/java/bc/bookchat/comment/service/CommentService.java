844f9c7102a4163525783eaddc390beff4f187ef
ChoiWonYu <phillip07290@naver.com>
2023-08-17 17:56:19 +0900

    feat: create comment api

--- src/main/java/bc/bookchat/comment/service/CommentService.java
+++ src/main/java/bc/bookchat/comment/service/CommentService.java
@@ -1,7 +1,29 @@
 package bc.bookchat.comment.service;
 
+import bc.bookchat.board.entity.Board;
+import bc.bookchat.board.service.BoardService;
+import bc.bookchat.comment.CommentRepository;
+import bc.bookchat.comment.entity.Comment;
+import bc.bookchat.member.entity.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final BoardService boardService;
+    private final CommentRepository commentRepository;
+
+    public Comment createComment(UUID boardId, Member member, String content) {
+        Board board=boardService.getBoardDetail(boardId);
+        Comment comment = new Comment(content,member,board);
+
+        comment.addToBoard(board);
+
+        commentRepository.save(comment);
+        return comment;
+    }
 }
