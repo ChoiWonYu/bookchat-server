c69cf9c3cb24a9d8244a72bce44eb7c5aefe09ca
ChoiWonYu <phillip07290@naver.com>
2023-08-20 09:58:11 +0900

    feat: 이메일 정규식 추가

--- src/main/java/bc/bookchat/auth/controller/dto/LoginRequest.java
+++ src/main/java/bc/bookchat/auth/controller/dto/LoginRequest.java
@@ -1,6 +1,7 @@
 package bc.bookchat.auth.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class LoginRequest {
 
   @NotBlank(message="이메일은 필수 입력값입니다.")
+  @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$",message = "이메일 형식에 맞지 않습니다.")
   private String email;
 
   @NotBlank(message="비밀번호는 필수 입력값입니다.")
