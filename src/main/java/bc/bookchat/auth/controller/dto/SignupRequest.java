6d4df2138f765a65566a8f5f9e5162d4a0610877
ChoiWonYu <phillip07290@naver.com>
2023-08-20 15:54:33 +0900

    feat: 로그인, 회원가입 길이 유효성 검사 추가

--- src/main/java/bc/bookchat/auth/controller/dto/SignupRequest.java
+++ src/main/java/bc/bookchat/auth/controller/dto/SignupRequest.java
@@ -1,31 +1,34 @@
 package bc.bookchat.auth.controller.dto;
 
 import bc.bookchat.member.entity.Member;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class SignupRequest {
 
-  @NotBlank(message="이메일은 필수 입력값입니다.")
-  @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$",message = "이메일 형식에 맞지 않습니다.")
-  private String email;
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "이메일 형식에 맞지 않습니다.")
+    private String email;
 
-  @NotBlank(message="이름은 필수 입력값입니다.")
-  private String userName;
+    @NotBlank(message = "이름은 필수 입력값입니다.")
+    @Size(min = 2, max = 10, message = "이름은 2자 이상, 10자 이하여야 합니다.")
+    private String userName;
 
-  @NotBlank(message="비밀번호는 필수 입력값입니다.")
-  private String password;
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 4, max = 16, message = "비밀번호는 4자 이상, 16자 이하여야 합니다.")
+    private String password;
 
-  public Member toEntity(){
-    return Member.builder()
-        .email(email)
-        .userName(userName)
-        .password(password)
-        .build();
-  }
+    public Member toEntity() {
+        return Member.builder()
+                .email(email)
+                .userName(userName)
+                .password(password)
+                .build();
+    }
 
 }
